ae11186d8ebcd347637e2ca1ad4b6c551b749502
nanditapanchal <nanditapanchalg@gmail.com>
2025-11-04 13:27:47 +0530

    connect server port and add public folder for static files

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json({ limit: "100kb" }));
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(express.static("public"));
+
+export { app };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,16 @@
-console.log("hi");
+
+import connectDB from "./config/db.js";
+import { app } from "./app.js";
+
+dotenv.config();
+
+connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
